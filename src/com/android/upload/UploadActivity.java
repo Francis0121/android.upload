13ef35a60e80a3b0505ef5e4cb6654be4d9f78a4
Francis0121 <tjdrms0121@gmail.com>
2015-01-12 18:00:02 +0900

    file directory access and ... not working

--- src/com/android/upload/UploadActivity.java
+++ src/com/android/upload/UploadActivity.java
@@ -1,9 +1,33 @@
 package com.android.upload;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class UploadActivity extends Activity {
+
+    int serverResponseCode = 0;
+    
+    Button button;
+    TextView textView;
+    ProgressDialog progressDialog = null;
+    
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +35,141 @@ public class UploadActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        textView.setText("File Upload");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {  
+                progressDialog = ProgressDialog.show(UploadActivity.this, "", "Uploading file ... ", true);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView.setText("Uploading started ... ");
+                            }
+                        });
+
+                        Handler mHandler = new Handler(Looper.getMainLooper());
+                        mHandler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                int response= upload();
+                                System.out.println("RES : " + response);
+                                if(response == 0){
+                                    runOnUiThread(new Runnable() {
+                                        public void run() {
+                                            textView.setText("Upload failed");
+                                            Toast.makeText(UploadActivity.this, "Upload failed.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                    progressDialog.dismiss();
+                                }
+                            }
+                        }, 0);
+
+                    }
+                }).start();
+            }
+        });
+        
     }
+
+    public int upload() {
+        String sourceFileUri = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)+"/Camera/02.jpg";
+        String upLoadServerUri = "http://192.168.0.43:9080";
+        String fileName = sourceFileUri;
+        
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        
+        Log.i("Upload file path", sourceFileUri);
+        File sourceFile = new File(sourceFileUri);
+        
+        if (!sourceFile.isFile()) {
+            Log.e("uploadFile", "Source File Does not exist");
+            return 0;
+        }
+        try { // open a URL connection to the Servlet
+            FileInputStream fileInputStream = new FileInputStream(sourceFile);
+            URL url = new URL(upLoadServerUri);
+            conn = (HttpURLConnection) url.openConnection(); // Open a HTTP  connection to  the URL
+            conn.setDoInput(true); // Allow Inputs
+            conn.setDoOutput(true); // Allow Outputs
+            conn.setUseCaches(false); // Don't use a Cached Copy
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            conn.setRequestProperty("file", fileName);
+            dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"file\";filename=\""+ fileName + "\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available(); // create a buffer of  maximum size
+
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            // read file and write it into form...
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            // send multipart form data necesssary after file data...
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // Responses from the server (code and message)
+            serverResponseCode = conn.getResponseCode();
+            String serverResponseMessage = conn.getResponseMessage();
+
+            Log.i("uploadFile", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);
+            if(serverResponseCode == 200){
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        textView.setText("File Upload Completed.");
+                        Toast.makeText(UploadActivity.this, "File Upload Complete.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            //close the streams //
+            fileInputStream.close();
+            dos.flush();
+            dos.close();
+
+        } catch (MalformedURLException ex) {
+            progressDialog.dismiss();
+            ex.printStackTrace();
+            Toast.makeText(UploadActivity.this, "MalformedURLException", Toast.LENGTH_SHORT).show();
+            Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+        } catch (Exception e) {
+            progressDialog.dismiss();
+            e.printStackTrace();
+            Toast.makeText(UploadActivity.this, "Exception : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.e("Upload file to server Exception", "Exception : " + e.getMessage(), e);
+        }
+        progressDialog.dismiss();
+        return serverResponseCode;
+    }
+
 }
