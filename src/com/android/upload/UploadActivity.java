0226e99306f0bf0fc6e61839100ced8e32cf0fbf
Francis0121 <tjdrms0121@gmail.com>
2015-01-12 16:30:06 +0900

    Initial commit

---
+++ src/com/android/upload/UploadActivity.java
@@ -0,0 +1,15 @@
+package com.android.upload;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UploadActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
